bfa9ee8948a0d97c20b1b4b1e0d3229a50c2355e
svniko <ya.pepulya@gmail.com>
2023-04-24 18:34:57 +0300

    bags fixed in checking schemas

--- main.py
+++ main.py
@@ -44,52 +44,50 @@ def main():
            {'title_basics_df': title_basics_df},
            {'principals_df': principals_df},
            {'names_df': names_df},
-           {'titles_df': titles_df}]
+           {'ratings_df': ratings_df},
+           {'episode_df': episode_df}]
     for df in dfs:
         for key, value in df.items():
             print(f'df: {key}')
             value.printSchema()
             print('--------')
 
-    # # task1 OK
+    # # task1
     task_1 = tasks.task1(akas_df)
     # # # Show results
     # task_1.show()
 
-    # # task2 OK
+    # # task2
     task_2 = tasks.task2(name_basics_df)
     # # # Show results
     # task_2 .show()
 
-    # # task 3 OK
+    # # task 3
     task_3 = tasks.task3(title_basics_df)
     # # Show results
-    task_3.show()
+    # task_3.show()
 
-    # task4 OK
+    # task4
     task_4 = tasks.task4(principals_df, names_df, title_basics_df)
     # # Show results
     # task_4.show()
     # # Save results
 
 
-    # # task5 OK
+    # # task5
     task_5 = tasks.task5(akas_df, title_basics_df)
     # # Show results
     # task_5.show()
-
 
     # # tasks6
     task_6 = tasks.task6(title_basics_df, episode_df)
     # # Show results
     # task_6.show()
 
-
     # tasks7
     task_7 = tasks.task7(spark_session, title_basics_df, ratings_df)
     # Show results
     # task_7.show()
-
 
     # tasks8
     task_8 = tasks.task8(spark_session, title_basics_df, ratings_df)
